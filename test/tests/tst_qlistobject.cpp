8fc5ec2d7c03ba7f1282bfa2d3baa5f01e4b0fde
HerikLyma <heriklyma@hotmail.com>
2017-01-29 20:30:25 -0200

    README.md now redirects to isocpp and was implemented a unit test for QListObject

--- test/tests/tst_qlistobject.cpp
+++ test/tests/tst_qlistobject.cpp
@@ -2,5 +2,16 @@
 
 void TST_QListObject::init()
 {
+    CWF::QListObject listObj;
 
+    listObj.add(new QObject);
+    listObj.add(new QObject);
+
+    QVERIFY2(listObj.size() == 2, "Should be equal 2");
+    QObject *obj = listObj[0];
+    QVERIFY2(obj != nullptr, "Should be different nullptr");
+    listObj.remove(obj);
+    QVERIFY2(listObj.size() == 1, "Should be equal 1");
+    listObj.setAutoDelete(true);
+    QVERIFY2(listObj.getAutoDelete() == true, "Should be equal true");
 }
