a01018cb4d17cd81ba0a7674a8ff34620092aa56
camilalyma <camilalymasa@hotmail.com>
2017-01-29 23:04:52 -0200

    First tests for QMapThreadSafety

--- test/tests/tst_qmapthreadsafety.cpp
+++ test/tests/tst_qmapthreadsafety.cpp
@@ -2,5 +2,12 @@
 
 void TST_QMapThreadSafety::test()
 {
-
+    CWF::QMapThreadSafety<int, QString> myMap;
+    myMap.insert(1, "First");
+    myMap.insert(2, "Second");
+    QVERIFY2(myMap.size() == 2, "Should return 2");
+    QVERIFY2(myMap.contains(1) == true, "Should return true");
+    myMap.remove(1);
+    QVERIFY2(myMap.size() == 1, "Should return 1");
+    QVERIFY2(myMap.contains(1) == false, "Should return false");
 }
