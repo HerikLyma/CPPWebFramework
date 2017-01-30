960a6332a3498438a572cf9cf3a53381159bccee
camilalyma <camilalymasa@hotmail.com>
2017-01-29 23:55:28 -0200

    Upgrade tests for Configuration

--- test/tests/tst_configuration.cpp
+++ test/tests/tst_configuration.cpp
@@ -5,7 +5,9 @@ void TST_Configuration::test()
     CWF::Configuration configuration("");
     configuration.setHost(QHostAddress("127.0.0.1"));
     configuration.setPort(8080);
+    configuration.setDomain("www.test.com.xyz");
 
     QVERIFY2(configuration.getHost().toString() == "127.0.0.1", "Should be 127.0.0.1");
     QVERIFY2(configuration.getPort() == 8080, "Should be 8080");
+    QVERIFY2(configuration.getDomain() == "www.test.com.xyz", "Should be www.test.com.xyz");
 }
