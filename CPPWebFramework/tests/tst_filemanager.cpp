9ff8014e41d0fd661a3b4ef0f8f32f2cc627c77a
HerikLyma <heriklyma@hotmail.com>
2017-01-30 22:12:21 -0200

    Increase in test coverage FileManager.

--- CPPWebFramework/tests/tst_filemanager.cpp
+++ CPPWebFramework/tests/tst_filemanager.cpp
@@ -17,6 +17,11 @@ void TST_FileManager::test()
     QVERIFY2(file == "index.html", "Should be index.html");
     QVERIFY2(fileManager.extract(file, '.') == "html", "Should be html");
 
+    file = "/config/" + file;
+    QVERIFY2(fileManager.fileName(file) == "index.html", "Should be index.html");
+
     QFile::FileError fileErro;
     QVERIFY2(fileManager.readAll(QDir::currentPath() + "/server/config/CPPWeb.ini", fileErro).size() > 0, "Should be > 0");
+    QVERIFY2(fileManager.readAll("xxxCPPWeb.ini", fileErro) == "No such file or directory", "Should be No such file or directory");
+    QVERIFY2(fileErro == QFile::FileError::OpenError, "Should be QFile::FileError::OpenError");
 }
