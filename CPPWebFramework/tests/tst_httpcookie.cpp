c8f2053db0f76601564da72c1ff371622dd7def1
HerikLyma <heriklyma@hotmail.com>
2017-01-31 00:01:53 -0200

    Increasing test coverage for HttpCookie and creating unit tests for the Properties class.

--- CPPWebFramework/tests/tst_httpcookie.cpp
+++ CPPWebFramework/tests/tst_httpcookie.cpp
@@ -12,7 +12,7 @@ void TST_HttpCookie::test()
         QVERIFY2(cookie.getComment() == "test", "Should be test");
         QVERIFY2(cookie.getDomain() == "www.test.com.xyz", "Should be www.test.com.xyz");
         QVERIFY2(cookie.getMaxAge() == 30, "Should return 30");        
-        QVERIFY2(cookie.getName() == "Name", "Should be Herik Lima de Castro");
+        QVERIFY2(cookie.getName() == "Name", "Should be Name");
         QVERIFY2(cookie.getPath() == "/home/Test/server", "Should be /home/Test/server");
         QVERIFY2(cookie.getSecure() == true, "Should return true");
         QVERIFY2(cookie.getValue() == "Test Cookie", "Should be Test Cookie");
@@ -20,6 +20,10 @@ void TST_HttpCookie::test()
         QVERIFY2(cookie.toByteArray() == "Name=Test Cookie; Comment=test; Domain=www.test.com.xyz; Max-Age=30; Path=/home/Test/server; Secure; Version=1",
                  "Shoud be Name=Test Cookie; Comment=test; Domain=www.test.com.xyz; Max-Age=30; Path=/home/Test/server; Secure; Version=1");
     }
+
+    CWF::HttpCookie cookie("Name", "Test Cookie");
+    QVERIFY2(cookie.getName() == "Name", "Should be Name");
+    QVERIFY2(cookie.getValue() == "Test Cookie", "Should be Test Cookie");
 }
 
 
--- CPPWebFramework/tests/tst_properties.cpp
+++ CPPWebFramework/tests/tst_properties.cpp
@@ -2,5 +2,7 @@
 
 void TST_Properties::test()
 {
-
+    CWF::Properties properties("TST_Properties.test()");
+    QVERIFY2(properties.m_class  == "TST_Properties", "Should be TST_Properties");
+    QVERIFY2(properties.m_method == "test()", "Should be test().");
 }
